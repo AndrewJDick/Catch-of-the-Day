192f585a20f71ac0351c25ca4a20eb7b1eab74ce
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-01-19 16:30:54 +0000

    video 9: created match, miss browser router targets. Added a 404 redirect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Dependancies
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
 // Import styles via Webpack
 import './css/style.css';
@@ -8,5 +9,20 @@ import './css/style.css';
 // Components
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App/>, document.querySelector('#main')); 
+// Component
+const Root = () => {
+  return (
+    <BrowserRouter>
+      { /* Looks like Match cannot be a direct child of BrowserRouter */ }
+      <div>
+        <Match exactly pattern='/' component={ StorePicker } />
+        <Match pattern='/store/:storeId' component={ App } />
+        <Miss component={ NotFound }/>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'));
