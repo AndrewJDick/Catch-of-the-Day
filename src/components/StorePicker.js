5a98457217d172ae64264330c5f92e31056d8d9a
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-01-19 14:03:43 +0000

    Video 4 - Storepicker module and example jsx comment

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
-	render() {
-		return <p>Hello!</p>
-	}
+  render() {
+    return (
+      <form className="store-selector">
+        { /* Example .jsx comment */ }
+        <h2> Please Enter A Store </h2>
+        <input type='text' required placeholder='Store Name' />
+        <button type='submit'> Visit Store </button>
+      </form>
+    )
+  }
 }
 
 export default StorePicker;
