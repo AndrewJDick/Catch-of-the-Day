528a343a8f33efcdca99509cba12895b2088f35d
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-01-19 13:39:30 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Dependancies
+import React from 'react';
+import { render } from 'react-dom';
+
+// Components
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker/>, document.querySelector('#main')); 
