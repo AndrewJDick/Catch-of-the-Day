281857ebc7a68a89d8bab4734d6d9520460f4233
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-01-26 15:53:21 +0000

    video 11: Bound the goToStore method to this to store the store picker value

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,25 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore(event) {
+
+    // Regular JS to prevent the default page reload on form submit.
+    event.preventDefault();
+
+    // First grab the text from the box...
+    console.log('You changed the url');
+
+    // ...then transition from '/' to '/store/:storeId'
+    console.log(this.storeInput.value);
+
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={ (e) => this.goToStore(e)} >
         { /* Example .jsx comment */ }
         <h2> Please Enter A Store </h2>
-        <input type='text' placeholder='Store Name' defaultValue={ getFunName() } required />
+        <input type='text' placeholder='Store Name' defaultValue={ getFunName() } ref={ (input) => { this.storeInput = input} } required />
         <button type='submit'> Visit Store </button>
       </form>
     )
