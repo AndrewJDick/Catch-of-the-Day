beb501effa8cf42fb0591ed189d29c74e8aee26d
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-02-09 11:46:23 +0000

    video 13: Added global state to the App class. Submitted addFish form now updates App.state.fishes

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,37 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+
+    // Required in the constructor
+    super();
+
+    // Binds the addFish method to App when it is initialised
+    this.addFish = this.addFish.bind(this);
+
+    // Define the initial state when App is initialized
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+
+  addFish(fish) {
+
+    // Store the current state of fishes and a timestamp(ms)
+    const fishes = { ...this.state.fishes };
+    const timestamp = Date.now();
+
+    // Add the fish to the fishes state
+    fishes[`fish-${timestamp}`] = fish;
+
+    // Update the fishes state. Important to update only what is required, rather than the global state
+    // ES6 shorthand for this.setState(fishes = fishes)
+    this.setState({fishes});
+
+    console.log(this);
+  }
+
   render() {
     return(
       <div className='catch-of-the-day'>
@@ -11,7 +42,7 @@ class App extends React.Component {
           <Header tagline='Fresh Seafood Market'/>
         </div>
         <Order/>
-        <Inventory/>
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
