ea0b42676004ae17eef6d1006f0ccf354ce22ea7
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-02-28 14:56:21 +0000

    video 18: fish inventory is now stored and synced with firebase

---
+++ src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyD-EAD1QOZV7QjnIl_yqmGok8XFSEXCwAU",
+  authDomain: "catch-of-the-day-6cd0d.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-6cd0d.firebaseio.com"
+});
+
+export default base;
+
